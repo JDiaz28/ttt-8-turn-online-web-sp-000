22444d61c614fa19e9320f57336c6b071a144a06
Abraham Moreno <adiaz2821@gmail.com>
2020-05-25 22:31:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,5 +31,9 @@ def turn(board)
   puts "Please enter 1-9:"
   answer = gets.chomp
   answer = input_to_index(answer)
-  if valid_move?(board, index) == 
+  if valid_move?(board, index)
+    move(board, answer)
+      puts display_board(board)
+  else
+  end 
 end 
